80ed2fad0850a8babf63a8f33ba5d46fbbd0541a
Jeressia Williamson <jeressiajaytv@gmail.com>
2019-10-13 12:51:19 -0500

    Add project files.

---
+++ C Sharp Review/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace C_Sharp_Review
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your word:");
+            var response = Console.ReadLine();
+            var noSpaces= response.Replace(" ", "");
+            Console.WriteLine($"You entered: {noSpaces}");
+
+            var allCharacters = "";
+
+            for (int i = 0; i < noSpaces.Length; i++)
+            {
+                int j = 0; var temp = "";
+                while (j <= i )
+                {
+                    temp += string.Concat((j == 0) ? char.ToUpper(noSpaces[i]) : char.ToLower(noSpaces[i]));
+                    j++;
+                }
+                allCharacters += temp + '-';
+            }
+            var finishedProduct = allCharacters.TrimEnd('-');
+            Console.WriteLine($"{finishedProduct}");
+
+        }
+    }
+}
